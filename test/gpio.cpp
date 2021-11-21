d99b1d204d1798d8ee6406a7cf07e2b0cdd68ebd
Jin <jaehwanspin@gmail.com>
2021-11-21 18:01:53 +0900

    test modified

--- test/gpio.cpp
+++ test/gpio.cpp
@@ -3,74 +3,28 @@
 #include <thread>
 #include <array>
 
-#include <boost/asio.hpp>
+#include <boost/signals2.hpp>
 
-#include <linux/gpio.h>
-
-#include <lpl/gpio.hpp>
-#include <lpl/soft_pwm.hpp>
-#include <lpl/epoll/epoll.hpp>
+#include <lpl/peripheral/gpio.hpp>
 
 int main(int argc, char **argv)
 {
-    // lpl::gpio gpio(200);
-    // gpio.set_direction(lpl::direction_t::out);
+    lpl::gpio led(200, lpl::direction_t::out);
+    lpl::gpio button(149, lpl::direction_t::in);
+    button.set_edge(lpl::edge_t::falling);
 
-    // bool value = false;
+    lpl::epoll::epoll epoll(button.value_fd(),
+        lpl::epoll::events::et);
 
-    // for (int i = 0; i < 200; i++)
-    // {
-    //     int ret = gpio.write(value);
-    //     value = !value;
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    // }
-
-    // boost::asio::io_context io_ctx;
-    // boost::asio::serial_port sp(io_ctx);
-
-    // sp.open(argv[1]);
-
-    // auto is_open = sp.is_open();
-    // if (!is_open) std::cerr << "fuck " << argv[1] << std::endl; 
-
-    // boost::asio::serial_port::baud_rate br(115200);
-    // sp.set_option(br);
-
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     std::string fucking = "oioi!!!!\r\n";
-    //     boost::asio::write(sp, boost::asio::buffer(fucking));
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    // }
-
-    lpl::gpio gpio(149);
-    gpio.set_direction(lpl::direction_t::in);
-    gpio.set_edge(lpl::edge_t::falling);
-
-    std::function<void(int)> fn =
-        [&](int fucking) -> void { std::cout << "ah" << fucking << std::endl; };
-
-
-
-    // lpl::epoll::epoll epoll(gpio.value_fd());
-
-    // lpl::soft_pwm pwm(149);
-    // pwm.enable();
-    boost::asio::io_context io_ctx;
-    boost::asio::ip::tcp::socket sock(io_ctx);
-    std::array<int, 256> buf;
-    sock.async_read_some(boost::asio::buffer(buf), [&](boost::system::error_code err, size_t s) {
-        
-
-        });
-
-    while (true)
+    auto now = std::chrono::high_resolution_clock::now();
+    while (std::chrono::high_resolution_clock::now() - now <= std::chrono::seconds(10))
     {
-        // epoll.wait();
-        // std::cout << static_cast<int>(gpio.read()) - 48 << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        epoll.wait(1);
+        auto value = static_cast<bool>(button.value());
+        led.write(value);
     }
 
+    
 
     return 0;
 }
--- test/soft_pwm.cpp
+++ test/soft_pwm.cpp
@@ -1,6 +1,47 @@
+#include <iostream>
 
+#include <lpl/peripheral/soft_pwm.hpp>
 
 int main(int argc, char** argv)
 {
+    using namespace std::literals;
+
+    lpl::soft_pwm sp(168);
+    sp.set_period(1'000'000);
+    sp.set_duty_cycle(0);
+    sp.set_enable(true);
+
+    uint64_t duty = 0;
+
+    for (int i = 0; i <= 1'000'000; i += 100'000)
+    {
+        sp.set_duty_cycle(i);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    for (int i = 1'000'000; i >= 0; i -= 100'000)
+    {
+        sp.set_duty_cycle(i);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    
+
+    // while (true)
+    // {
+    //     for (int i = 0; i <= 1000; i++)
+    //     {
+    //         duty += i * 1'000;
+    //         sp.set_duty_cycle(duty);
+    //         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //     }
+    //     for (int i = 1000; i >= 0; i--)
+    //     {
+    //         duty -= i * 1'000;
+    //         sp.set_duty_cycle(duty);
+    //         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //     }
+    // }
+
     return 0;
 }
